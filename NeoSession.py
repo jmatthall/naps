11e2ca399f2e57034f5d7d01dc87de931e981e12
jmatthall <jhall199p@gmail.com>
2016-12-27 20:51:45 -0500

    Add files via upload

--- NeoSession.py
+++ NeoSession.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
-'''
-Mimics a neopets websession, handles cookies, headers, updating referer
-header and human-like pauses.
-
-Part of naps (neopets automation program suite)
-'''
-
+'''Neopets login.'''
 
 import requests
 import pickle
@@ -35,17 +29,17 @@ class NeoSession:
         self.update_session_cookies()
 
     def session_get(self, url):
-        time.sleep(random.randint(5, 11))
-        return self.session.get(url)
+        response =  self.session.get(url)
         self.session.headers.update({'Referer': url})
+		return response
 
     def session_post(self, url, data=None):
-        time.sleep(random.randint(5, 11))
         if data is None:
-            return self.session.post(url)
+            html = self.session.post(url)
         else:
-            return self.session.post(url, data)
+            html = self.session.post(url, data)
         self.session.headers.update({'Referer': url})
+		return html
 
     def update_session_cookies(self):
         if os.path.isfile(self.jar):
@@ -63,38 +57,55 @@ class NeoSession:
             session_headers = json.load(headers)
             self.session.headers.update(session_headers)
 
-    def check_login(self):
-        url = 'http://www.neopets.com/'
-        resp = self.session_get(url)
-        if 'Welcome, <a href="/userlookup.phtml?user={}">'.format(
-                self.username) in resp.text:
+    def check_login(self, resp):
+
+
+        if 'Welcome, <a href="/userlookup.phtml?user={}">'.format(self.username) in resp:
             print('Already logged in.')
             return True
 
     def login(self):
         '''Log-in to neopets.com'''
-        if self.check_login() is not True:
-            url = 'http://www.neopets.com/login'
-            resp = self.session_get(url)
-            self.session.headers.update({'Referer': url})
 
-            try:
-                resp.raise_for_status()
-            except requests.exceptions.HTTPError:
-                print('Could not connect to neopets.com')
-                sys.exit(1)
+        url = 'http://www.neopets.com/login'
+        resp = self.session_get(url)
+		self.session.headers.update({'Referer': url})
+		try:
+			resp.raise_for_status()
+		except requests.exceptions.HTTPError:
+			print('Could not connect to neopets.com')
+			sys.exit(1)
+        if self.check_login(resp) is not True:
+        	url = 'http://www.neopets.com/login.phtml'
+        	self.session_post(url, self.login_data)
+        	print('Login successful.')
 
-            if os.path.isfile(self.jar) is not True:
-                os.system('touch neopets.cookies')
-                self.session_cookies = resp.cookies
-                return self.session_cookies
-            url = 'http://www.neopets.com/login.phtml'
-            self.session_post(url, self.login_data)
-            print('Login successful.')
+		return
+#  you can deal with this
+
+#if os.path.isfile(self.jar) is not True:
+ #           	os.system('touch neopets.cookies')
+   #         	self.session_cookies = resp.cookies
+  #         	return self.session_cookies
 
 
-def main():
-    NeoSession()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
